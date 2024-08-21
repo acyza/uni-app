96323860194068fe1dd9827b6d8c8abc2d535e1b
acyza <101238421+acyza@users.noreply.github.com>
2024-08-22 01:51:32 +0800

    inital

---
+++ src/components/SlotDemo.vue
@@ -0,0 +1,24 @@
+<template>
+    <view class="wrapper">
+        <view v-for="item,index of data" :key="index">
+            <view>
+                <slot :data="item" />
+            </view>
+        </view>
+        <view class="end"></view>
+    </view>
+</template>
+<script lang="ts" setup>
+const props = defineProps<{
+    getData?: Function
+    useDefaultData: boolean
+}>()
+
+const data = props.useDefaultData ? ['1','2','3'] : props.getData?.()
+</script>
+<style scoped>
+.wrapper{
+    width: 100%;
+    height: 100%;
+}
+</style>
---
+++ src/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="content">
+    <SlotDemo :useDefaultData="true">
+      <template #default="item">
+        <view @click="clickHandler(item)">{{ item }}</view>
+      </template>
+      </SlotDemo>
+  </view>
+</template>
+
+<script setup>
+import SlotDemo from '../../components/SlotDemo.vue';
+
+function clickHandler(e) {
+  console.log(e)
+}
+</script>
